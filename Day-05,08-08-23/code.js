9c78b60e958c0ab441f9e263f97314454271b92f
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-08-09 15:03:03 +0530

    save

--- Day-05,08-08-23/code.js
+++ Day-05,08-08-23/code.js
@@ -1,4 +1,6 @@
-let d7 = document.getElementById("D7");
-d7.addEventListener("click",function(){
-location.replace("http://127.0.0.1:5501/Day-05,08-08-23/GAMES/game_1.html");
-});
+// let d7 = document.getElementById("D7");
+// let l = document.getElementById("link");
+// d7.addEventListener("click",function(){
+// // location.replace("http://127.0.0.1:5501/Day-05,08-08-23/GAMES/game_1.html");
+    
+// });
