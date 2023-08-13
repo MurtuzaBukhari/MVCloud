6c6c7cf7f9889dea260265400fdf1625aa54c8bc
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-08-13 22:31:13 +0530

    save

--- Day-07,11-08-23/Task_16.java
+++ Day-07,11-08-23/Task_16.java
@@ -8,7 +8,7 @@ public class Task_16 {
         System.out.print("Enter the number of dice: ");
         int num_1 = scanner.nextInt();
 
-        System.out.print("Enter the output: ");
+        System.out.print("Enter the outcome: ");
         int num_2 = scanner.nextInt();
 
         int[][] array = new int[num_1 + 1][num_2 + 1];
@@ -17,12 +17,12 @@ public class Task_16 {
         for (int i = 1; i <= num_1; i++) {
             for (int j = 1; j <= num_2; j++) {
                 for (int k = 1; k <= 6 && k <= j; k++) {
-                    array[i][j] += array[i - 1][j - k];
+                    array[i][j] = array[i][j] + array[i - 1][j - k];
                 }
             }
         }
 
-        System.out.println("Number of combinations: " + array[num_1][num_2]);
+        System.out.println("Output"+ array[num_1][num_2]);
 
         scanner.close();
     }
