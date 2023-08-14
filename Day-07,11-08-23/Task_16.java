1090b727d93cc58a78f9b47bd851b156c68e2b5e
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-08-14 10:10:38 +0530

    save

--- Day-07,11-08-23/Task_16.java
+++ Day-07,11-08-23/Task_16.java
@@ -1,29 +1,36 @@
+
 import java.util.Scanner;
 
 public class Task_16 {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of dice: ");
-        int num_1 = scanner.nextInt();
-
-        System.out.print("Enter the outcome: ");
-        int num_2 = scanner.nextInt();
-
-        int[][] array = new int[num_1 + 1][num_2 + 1];
-        array[0][0] = 1;
-
-        for (int i = 1; i <= num_1; i++) {
-            for (int j = 1; j <= num_2; j++) {
-                for (int k = 1; k <= 6 && k <= j; k++) {
-                    array[i][j] = array[i][j] + array[i - 1][j - k];
-                }
-            }
+    public static int countWays(int num_1, int num_2) {
+        if (num_2 < num_1 || num_2 > num_1 * 6) {
+            return 0;
         }
 
-        System.out.println("Output"+ array[num_1][num_2]);
+        if (num_1 == 1 || num_2 == 0) {
+            return 1;
+        }
 
-        scanner.close();
+        int ways = 0;
+        for (int i = 1; i <= 6; i++) {
+            ways += countWays(num_1 - 1, num_2 - i);
+        }
+        return ways;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of dice: ");
+        int num_1=sc.nextInt();
+
+        System.out.print("Enter the desired outcome: ");
+        int num_2 = sc.nextInt();
+
+        int waysToGetOutcome = countWays(num_1, num_2);
+        System.out.println("Number of ways to get the outcome: " + waysToGetOutcome);
+
+        sc.close();
+    }
+
 }
